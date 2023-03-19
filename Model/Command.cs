89c423ff5b9b4115a754611eb77efa52947068d3
Ján Homola <xhomola@fi.muni.cz>
2023-03-19 09:36:16 +0100

    Enum for command implemented

---
+++ Model/Command.cs
@@ -0,0 +1,12 @@
+namespace HW02.Model;
+
+public enum Command
+{
+    AddProduct,
+    DeleteProduct,
+    ListProduct,
+    AddCategory,
+    DeleteCategory,
+    ListCategories,
+    GetProductsByCategory
+}
